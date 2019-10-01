a90f3d6a07ebfb119a61be9f07ef7c32d0023cb9
Josh Medeski <josh.medeski@gmail.com>
2019-10-01 11:41:58 -0500

    Add second route to demonstrate get

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/about", (req, res) => res.send("About"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
